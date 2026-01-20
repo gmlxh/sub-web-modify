ac40cddcdd454854f47728a42d7576ad9f24b47b
Austin <158451332+gmlxh@users.noreply.github.com>
2026-01-20 19:31:11 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,6 +338,7 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
+          "自用后端": "https://subapi.lianghuigu.com",
           "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
@@ -347,6 +348,7 @@ export default {
           "sub-web作者提供": "https://api.wcc.best",
         },
         backendOptions: [
+          { value: "https://subapi.lianghuigu.com" },
           { value: "https://subapi.cmliussss.net" },
           { value: "https://subapi.fxxk.dedyn.io" },
           { value: "https://url.v1.mk" },
@@ -357,6 +359,13 @@ export default {
         ],
         remoteConfig: [
           {
+            label: "自用规则",
+            options:[
+              {
+              label: "自用规则，随时更新",
+              value: "https://raw.githubusercontent.com/gmlxh/ACL4SSR/blob/main/ACL4SSR_Online_Full_Mannix.ini"
+            }
+                    ],
             label: "CM规则",
             options: [
               {
@@ -1310,3 +1319,4 @@ export default {
   }
 };
 </script>
+
